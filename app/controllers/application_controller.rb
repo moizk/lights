0ec2ea8a37fdb7da0cdba05c030ded849568b046
Moiz K. Malik <moiz@heuretix.com>
2014-06-27 22:41:25 -0400

    flash the first light hehehehe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :loop_one, :flash
+  def loop_one
+    client = Hue::Client.new
+    light = client.lights.third
+    light.set_state({:effect => 'colorloop'})
+  end
+
+  def flash
+    client = Hue::Client.new
+    light = client.lights.first
+    light.set_state({:alert => 'lselect'})
+  end
 end
